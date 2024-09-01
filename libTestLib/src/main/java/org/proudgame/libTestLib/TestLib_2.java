41378305bd6d59c08f3206a04e5a189309082ad7
yuhanxun <yuhanxun@126.com>
2024-09-01 14:37:19 +0800

    msg

---
+++ libTestLib/src/main/java/org/proudgame/libTestLib/TestLib_2.java
@@ -0,0 +1,24 @@
+/**
+ * author: Crashxun
+ * created on: 2024/9/1 14:27
+ * description:
+ */
+
+package org.proudgame.libTestLib;
+
+/**
+ *
+ */
+public class TestLib_2 {
+
+    /**
+     * test method
+     * @param a arg1
+     * @param b arg2
+     */
+    public void method(int a, int b) {
+
+    }
+
+
+}
